c2e54d9edc6868c7cdb7db2bc7a3d33875cc40e2
Andre Cupini <andrecupini@gmail.com>
2022-12-22 17:27:37 -0300

    Config Map

--- assets/server.go
+++ assets/server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -17,4 +20,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %s ", err)
+	}
+
+	fmt.Fprintf(w, "My ffamily: %s", string(data))
 }
